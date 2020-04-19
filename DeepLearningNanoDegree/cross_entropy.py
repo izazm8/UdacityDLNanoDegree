60a4438ffac64f007e04d5ceb6365f065df5c12a
izazm8 <izazm8@gmail.com>
2020-04-19 12:08:19 +0500

    used numpy to calcluate corss entropy

--- DeepLearningNanoDegree/cross_entropy.py
+++ DeepLearningNanoDegree/cross_entropy.py
@@ -3,7 +3,6 @@ import numpy as np
 # Write a function that takes as input two lists Y, P,
 # and returns the float corresponding to their cross-entropy.
 def cross_entropy(Y, P):
-    c_entropy = 0.0
-    for i in range(len(Y)):
-        c_entropy+= -( (Y[i]*np.log(P[i])) + ((1-Y[i])*np.log(1-P[i])) )
-    return c_entropy
+    Y=np.float_(Y)
+    P=np.float_(P)
+    return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
