ba19cafd40ad230b50855feb4074b1d97860f2c7
izazm8 <izazm8@gmail.com>
2020-04-19 12:05:31 +0500

    Added code to calculate cross entropy

--- DeepLearningNanoDegree/cross_entropy.py
+++ DeepLearningNanoDegree/cross_entropy.py
@@ -3,4 +3,7 @@ import numpy as np
 # Write a function that takes as input two lists Y, P,
 # and returns the float corresponding to their cross-entropy.
 def cross_entropy(Y, P):
-    pass
+    c_entropy = 0.0
+    for i in range(len(Y)):
+        c_entropy+= -( (Y[i]*np.log(P[i])) + ((1-Y[i])*np.log(1-P[i])) )
+    return c_entropy
